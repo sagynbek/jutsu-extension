231474571b058ae988850de2e48b53b8b7b9af35
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-12-13 14:31:29 +0300

    Configurations added to popup

--- src/ui/popup/index.ts
+++ src/ui/popup/index.ts
@@ -1,3 +1,9 @@
+import { OPEN_NEXT_VIDEO, SKIP_VIDEO_INTRO } from "inject/constants";
+import { UserPreference } from "inject/models/user-preference";
+
+
+const userPreference = new UserPreference();
+
 function localizeHtmlPage() {
   // Localize by replacing __MSG_***__ meta tags
   const objects = document.getElementsByTagName("html");
@@ -15,35 +21,30 @@ function localizeHtmlPage() {
   }
 }
 
-function styleThemeSwitch(theme) {
-  theme = theme || "dark";
-
-  if (theme === "dark") {
-    document.getElementById("enableDarkTheme").classList.add("active");
-    document.getElementById("disableDarkTheme").classList.remove("active");
-  }
-  else {
-    document.getElementById("enableDarkTheme").classList.remove("active");
-    document.getElementById("disableDarkTheme").classList.add("active");
-  }
+function onCheckboxChange(e, key: string) {
+  const isChecked = e.target.checked;
+  userPreference.set(key, isChecked);
 }
 
-function toggleDarkTheme(enabled) {
-  const theme = enabled ? "dark" : "default";
-  console.log(theme);
-  chrome.storage.sync.set({ theme }, () => {
-    styleThemeSwitch(theme);
-  });
+function setCheckboxValue(checkbox: HTMLElement, value) {
+  if (value) { checkbox.setAttribute("checked", "true"); }
+  else { checkbox.removeAttribute("checked"); }
 }
 
-(() => {
-  chrome.storage.sync.get(["theme"], function (result) {
-    console.log(result);
-    styleThemeSwitch(result.theme);
-  });
-
+(async () => {
   localizeHtmlPage();
 
-  document.getElementById("enableDarkTheme").addEventListener("click", function () { toggleDarkTheme(true); });
-  document.getElementById("disableDarkTheme").addEventListener("click", function () { toggleDarkTheme(false); });
+  const skipRecapElement = document.getElementById("skip-recap");
+  const nextEpisodeElement = document.getElementById("next-episode");
+
+
+  const shouldSkip = await userPreference.get(SKIP_VIDEO_INTRO, true);
+  const shouldOpenNextEpisode = await userPreference.get(OPEN_NEXT_VIDEO, true);
+
+  setCheckboxValue(skipRecapElement, shouldSkip);
+  setCheckboxValue(nextEpisodeElement, shouldOpenNextEpisode);
+
+
+  skipRecapElement.addEventListener("change", (e) => { onCheckboxChange(e, SKIP_VIDEO_INTRO) });
+  nextEpisodeElement.addEventListener("change", (e) => { onCheckboxChange(e, OPEN_NEXT_VIDEO) });
 })();
