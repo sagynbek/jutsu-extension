750bc9887cb8faa5924af5f2b7992a1fe73f08bf
Sagynbek <kenzhebaevsagynbek@gmail.com>
2021-08-18 22:53:53 +0600

    F11 keypress does not exit auto full screen

--- src/inject/player/auto-full-screen.ts
+++ src/inject/player/auto-full-screen.ts
@@ -19,12 +19,7 @@ export class AutoFullScreen<T extends HTMLDivElement> extends BaseModel<T> {
   private async init() {
     await this.subscribeToPreference(FULL_SCREEN.key, FULL_SCREEN.default, this.setPermition);
     await this.subscribeToPreference(AUTO_PLAY_VIDEO.key, AUTO_PLAY_VIDEO.default, this.setAutoPlayPermition);
-    await this.subscribeToPreference(NEXT_EPISODE_LAST_TRIGGER_TIME.key, NEXT_EPISODE_LAST_TRIGGER_TIME.default, (lastTriggerTime) => {
-      // @ts-ignore
-      if (lastTriggerTime && (new Date() - new Date(lastTriggerTime)) < TOLERATE_AUTO_FULL_SCREEN_IN_MS) {
-        this.nextEpisodeLastTriggerTimeFitsTime = true;
-      }
-    });
+    await this.subscribeToPreference(NEXT_EPISODE_LAST_TRIGGER_TIME.key, NEXT_EPISODE_LAST_TRIGGER_TIME.default, this.handleLastTriggerTime);
 
     this.onDomAdd(this.validate, this.action);
   }
@@ -35,6 +30,13 @@ export class AutoFullScreen<T extends HTMLDivElement> extends BaseModel<T> {
 
   private setAutoPlayPermition = (permission) => {
     this.autoPlayPermitted = permission;
+  }
+
+  private handleLastTriggerTime = (lastTriggerTime) => {
+    // @ts-ignore
+    if (lastTriggerTime && (new Date() - new Date(lastTriggerTime)) < TOLERATE_AUTO_FULL_SCREEN_IN_MS) {
+      this.nextEpisodeLastTriggerTimeFitsTime = true;
+    }
   }
 
   private validate = (element: T) => {
@@ -53,8 +55,14 @@ export class AutoFullScreen<T extends HTMLDivElement> extends BaseModel<T> {
 
     const bodyElement = document.querySelector("body");
     bodyElement.classList.add(CSS_FULL_SCREEN_CLASS);
+
     document.addEventListener("mousedown", this.exitCssFullScreen);
-    document.addEventListener("keyup", this.exitCssFullScreen);
+    document.addEventListener("keyup", (e) => {
+      // F11's keycode = 122
+      if (e.keyCode !== 122) {
+        this.exitCssFullScreen();
+      }
+    });
   }
 
   private exitCssFullScreen = () => {
